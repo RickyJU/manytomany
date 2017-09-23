ddb153b049e362a2a8178f0e287ad0682292e099
RickyJuvianto <ricky.juvianto98@gmail.com>
2017-09-23 11:38:34 +0700

    Reading Data

--- manytomany/app/Http/routes.php
+++ manytomany/app/Http/routes.php
@@ -23,3 +23,13 @@ Route::get('/create', function(){
 	$role = new Role(['name'=>'Administrator']);
 	$user->roles()->save($role);
 });
+
+Route::get('/read', function(){
+	$user = User::findOrFail(1);
+	
+	//dd($user->roles);
+	foreach($user->roles as $role){
+		//dd($role);
+		echo $role->name;
+	}
+});
